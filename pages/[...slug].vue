dcca5da2d5897a0be0c3fcb05fc7bbed54c21a73
Shahlo Seidmedova <sseidmed@Shahlos-MacBook-Pro.local>
2024-08-19 16:51:44 -0400

    add table of contents

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -25,21 +25,45 @@
         </div>
       </NuxtLink>
 
-      <div>
-        <ContentRenderer
-          v-if="post"
-          :value="post"
-        >
-          <h1>{{ post?.title }}</h1>
-          <ContentRendererMarkdown :value="post" />
-          <template #not-found>
-            <h1>Document not found</h1>
-          </template>
-        </ContentRenderer>
+      <div class="flex mt-12">
+        <div class="flex-3 flex flex-col w-4/6">
+          <ContentRenderer
+            v-if="post"
+            :value="post"
+          >
+            <h1>{{ post?.title }}</h1>
+            <ContentRendererMarkdown :value="post" />
+            <template #not-found>
+              <h1>Document not found</h1>
+            </template>
+          </ContentRenderer>
+        </div>
+
+        <div class="flex-1 ml-4">
+          <div class="sticky top-0 bg-gray-900 rounded-lg shadow-md p-2 flex flex-col items-center prose prose-a:text-pink-600 hover:prose-a:text-blue-500 z-10 w-80">
+            <div class="text-xl font-semibold text-white my-4">
+              TABLE OF CONTENTS
+            </div>
+            <ul class="list-disc pl-5 text-blue-800">
+              <li
+                v-for="link of post?.body?.toc?.links"
+                :key="link.id"
+                class="no-underline hover:underline"
+              >
+                <a
+                  :href="`#${link.id}`"
+                  class="text-blue-600 hover:text-blue-800 transition-colors duration-300"
+                >
+                  {{ link.text }}
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
 
 
-      <!-- <div>
+    <!-- <div>
         <ContentDoc
           v-slot="{doc}"
           :path="slug ? `/blog/${slug[0]}` : '/blogPage'"
@@ -63,14 +87,14 @@
         </ContentDoc>
       </main> -->
 
-      <!-- <main class="prose text-left">
+    <!-- <main class="prose text-left">
         <ContentRenderer
           v-if="data"
           :value="data"
         />
       </div> -->
 
-      <!-- <div>
+    <!-- <div>
         <h1>Title: {{ post?.title }}</h1>
         <MDC
           v-if="post"
@@ -79,7 +103,7 @@
         />
       </div> -->
 
-      <!-- <div>
+    <!-- <div>
         <ContentDoc :path="slug ? `/blogPage/${slug[0]}` : '/blog'">
           <template #default="{ doc }">
             <div class="flex mt-12">
