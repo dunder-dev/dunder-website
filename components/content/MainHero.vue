34cc9b99351d72c0e759982eba3958f324b114d4
reblevins <rodney.blevins@groundfloor.us>
2024-10-10 16:46:01 -0600

    Height

--- components/content/MainHero.vue
+++ components/content/MainHero.vue
@@ -2,9 +2,9 @@
   <div class="bg-fixed bg-[url('/images/network-8303719_1920.jpg')] bg-no-repeat bg-cover">
     <div class="md:bg-gradient-to-r from-gray-900 from-10% via-transparent to-gray-900 to-90% hover:bg-transparent">
       <div class="container mx-auto">
-        <div class="flex justify-center items-center h-screen md:h-96">
+        <div class="flex justify-center items-center h-[calc(100vh-86px)] md:h-96">
           <div class="text-center text-white">
-            <h1 class="text-4xl md:text-6xl font-bold">
+            <h1 class="text-6xl md:text-6xl font-bold">
               Dunder Dev
             </h1>
             <p class="text-xl md:text-2xl mt-4">
