60ef0882c6a046bdb3b90424d19df723ba33fdb2
reblevins <rodney.blevins@groundfloor.us>
2024-10-10 16:34:17 -0600

    Hero

---
+++ components/content/MainHero.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-fixed bg-[url('/images/network-8303719_1920.jpg')] bg-no-repeat bg-cover">
+    <div class="md:bg-gradient-to-r from-gray-900 from-10% via-transparent to-gray-900 to-90% hover:bg-transparent">
+      <div class="container mx-auto">
+        <div class="flex justify-center items-center h-screen md:h-96">
+          <div class="text-center text-white">
+            <h1 class="text-4xl md:text-6xl font-bold">
+              Dunder Dev
+            </h1>
+            <p class="text-xl md:text-2xl mt-4">
+              Learn AI, Blog, Courses, and more!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
