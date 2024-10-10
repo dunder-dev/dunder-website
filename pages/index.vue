4a27260f1ab6c179da5e6efa06ac230539a61a9e
reblevins <rodney.blevins@groundfloor.us>
2024-10-10 16:34:39 -0600

    Move it to its own component

--- pages/index.vue
+++ pages/index.vue
@@ -1,133 +1,14 @@
 <template>
   <div>
+    <MainHero />
+
+    <CTASignup />
+
     <div class="mt-12 flex flex-col">
       <h1 class="text-gray-200 mx-auto">
         Meet the Team
       </h1>
-      <div class="grid grid-cols-1 gap-y-8 pt-4 lg:mx-16 lg:max-w-none lg:grid-cols-2 lg:gap-x-8">
-        <div class="w-full max-w-md mx-auto mb-4 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-          <div class="md:flex lg:flex-col xl:flex-row xl:h-full">
-            <div class="md:shrink-0 lg:w-full xl:w-48">
-              <NuxtImg
-                class="h-96 w-full object-cover md:h-full md:w-48 lg:w-full"
-                src="/images/gwen_profile.jpg"
-                alt="Gwendolyn Faraday"
-              />
-            </div>
-            <div class="p-8">
-              <div class="uppercase tracking-wide text-xl text-indigo-500 font-semibold">
-                Gwendolyn Faraday
-              </div>
-              <a
-                href="#"
-                class="block mt-1 text-lg leading-tight text-black hover:underline"
-              >Gwendolyn is a professional software developer with 9 years of experience. She has worked as an ML engineer and is now an AI engineer specializing at the intersection of AI, architecture, and software.</a>
-              <!-- <p class="mt-2 text-slate-500">
-                Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.
-              </p> -->
-            </div>
-          </div>
-        </div>
-
-        <div class="w-full max-w-md mx-auto mb-4 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-          <div class="md:flex lg:flex-col xl:flex-row xl:h-full">
-            <div class="md:shrink-0 lg:w-full xl:w-48">
-              <NuxtImg
-                class="h-96 w-full object-cover md:h-full md:w-48 lg:w-full"
-                src="/images/daniel_profile.jpg"
-                alt="Daniel Lomelino"
-              />
-            </div>
-            <div class="p-8">
-              <div class="uppercase tracking-wide text-xl text-indigo-500 font-semibold">
-                Daniel Lomelino
-              </div>
-              <a
-                href="#"
-                class="block mt-1 text-lg leading-tight text-black hover:underline"
-              >Daniel was a highly-rated lead instructor for Python at a coding school for several years. With over 20 years of experience in software development, he brings a wealth of knowledge and teaching expertise to the table.</a>
-              <!-- <p class="mt-2 text-slate-500">
-                Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.
-              </p> -->
-            </div>
-          </div>
-        </div>
-
-
-        <div class="w-full max-w-md mx-auto mb-4 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-          <div class="md:flex lg:flex-col xl:flex-row xl:h-full">
-            <div class="md:shrink-0 lg:w-full xl:w-48">
-              <NuxtImg
-                class="h-96 w-full object-cover md:h-full md:w-48 lg:w-full"
-                src="/images/piero_profile.jpg"
-                alt="Piero Madar"
-              />
-            </div>
-            <div class="p-8">
-              <div class="uppercase tracking-wide text-xl text-indigo-500 font-semibold">
-                Piero Madar
-              </div>
-              <a
-                href="#"
-                class="block mt-1 text-lg leading-tight text-black hover:underline"
-              >Piero has decades of experience working in tech and served as a director of a coding school. He developed coding curricula and is an excellent teacher. Currently, he is heavily involved in the AI space.</a>
-              <!-- <p class="mt-2 text-slate-500">
-                Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.
-              </p> -->
-            </div>
-          </div>
-        </div>
-
-
-        <div class="w-full max-w-md mx-auto mb-4 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-          <div class="md:flex lg:flex-col xl:flex-row xl:h-full">
-            <div class="md:shrink-0 lg:w-full xl:w-48">
-              <NuxtImg
-                class="h-96 w-full object-cover md:h-full md:w-48 lg:w-full"
-                src="/images/rodney_profile.jpg"
-                alt="Rodney Blevins"
-              />
-            </div>
-            <div class="p-8">
-              <div class="uppercase tracking-wide text-xl text-indigo-500 font-semibold">
-                Rodney Blevins
-              </div>
-              <a
-                href="#"
-                class="block mt-1 text-lg leading-tight text-black hover:underline"
-              >Rodney is a software developer who thrives in fast-paced environments. He has a decade of experience working in Python and other languages, adding a lot of fun and flavor to our group.</a>
-              <!-- <p class="mt-2 text-slate-500">
-                Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.
-              </p> -->
-            </div>
-          </div>
-        </div>
-
-
-        <div class="w-full max-w-md mx-auto mb-4 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-          <div class="md:flex lg:flex-col xl:flex-row xl:h-full">
-            <div class="md:shrink-0 lg:w-full xl:w-48">
-              <NuxtImg
-                class="h-96 w-full object-cover md:h-full md:w-48 lg:w-full"
-                src="/images/shahlo_profile.jpg"
-                alt="Shahlo Seidmedova"
-              />
-            </div>
-            <div class="p-8">
-              <div class="uppercase tracking-wide text-xl text-indigo-500 font-semibold">
-                Shahlo Seidmedova
-              </div>
-              <a
-                href="#"
-                class="block mt-1 text-lg leading-tight text-black hover:underline"
-              >Shay hails from Turkmenistan and has 3 years of experience as a professional developer. She is very passionate about learning and coding, and is currently building language learning tools using AI.</a>
-              <!-- <p class="mt-2 text-slate-500">
-                Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.
-              </p> -->
-            </div>
-          </div>
-        </div>
-      </div>
+      <BioCards />
     </div>
   </div>
 </template>
