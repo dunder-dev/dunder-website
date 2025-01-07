fd43dbb941a5b5832b22c3f5e4cdad9ce166c391
Daniel Lomelino <dlomelino@pm.me>
2025-01-07 16:54:49 -0500

    Update copyright text Open footer links in new tab

--- layouts/Default.vue
+++ layouts/Default.vue
@@ -12,21 +12,19 @@
 
     <footer class="h-16">
       <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span class="text-sm sm:text-center dark:text-gray-400">2024 <a
-          href=""
-          class="hover:underline"
-        >Dunder Dev</a>
-        </span>
+        <span class="text-sm sm:text-center dark:text-gray-400">&copy; 2025 Developer Underground</span>
         <ul class="flex flex-wrap items-center mt-3 text-sm font-medium sm:mt-0 list-none">
           <li>
             <a
               href="https://www.linkedin.com/company/developer-underground"
+              target="_blank"
               class="hover:underline me-4 md:me-6"
             >LinkedIn</a>
           </li>
           <li>
             <a
               href="https://x.com/dunder_dev"
+              target="_blank"
               class="hover:underline me-4 md:me-6"
             >Twitter</a>
           </li>
